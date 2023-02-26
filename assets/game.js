cbd85bf187fbaf6ecf1635313a1a2e93b6d49cab
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-02-26 13:55:40 -0600

    add question array

--- assets/game.js
+++ assets/game.js
@@ -1,25 +1,70 @@
-var timeText = document.querySelector("#timeLeft");
-// timer starts
-var secondsLeft = 30;
+function playGame() {
+    // timer starts
+    var timeText = document.querySelector("#timeLeft");
 
-function countdown () {
-    var timeLeft = setInterval(function() {
+    var secondsLeft = 20;
+
+    function countdown () {
+        var timeLeft = setInterval(function() {
         secondsLeft--;
         timeText.textContent = secondsLeft;
 
         if(secondsLeft === 0) {
             clearInterval(timeLeft);
-        }
-    }, 1000);
-}
+          }
+        }, 1000);
+    }
+    countdown();
 
-countdown();
-// select question and choice el from html
+    // select question and choice el from html
+    var question = document.querySelector(".question");
+    var choices = document.querySelector(".choice-text");
+    // select points in score-display
+    var correctCount = document.querySelector("#correctCount");
 
-// select points and time remaining in score-display
-
-// create question array with question/choices/answer
-
+    // create question array with question/choices/answer
+    let questions = [
+        {
+            question:"Which of the following is NOT a primitive data type?",
+            choice1:"Number",
+            choice2:"Boolean",
+            choice3:"Letter",
+            choice4:"String",
+            answer:"3",
+        },
+        {
+            question:"Which method adds items to an array one at a time?",
+            choice1:"add()",
+            choice2:"push()",
+            choice3:"sort()",
+            choice4:"Cannot be done",
+            answer:"2",
+        },
+        {
+            question:"What kind of loop will repeat a block of code until a condition returns 'false'?",
+            choice1:"For Loop",
+            choice2:"For In Loop",
+            choice3:"Fruit Loop",
+            choice4:"While Loop",
+            answer:"4",
+        },
+        {
+            question:"What does the acronym DOM represent?",
+            choice1:"Dinner On Me",
+            choice2:"Document Object Model",
+            choice3:"Document Only Model",
+            choice4:"Done On Microsoft",
+            answer:"2",
+        },
+        {
+            question:"Which term describes reusable blocks of code that perform a certain task?",
+            choice1:"Function",
+            choice2:"Class",
+            choice3:"ID",
+            choice4:"Boolean",
+            answer:"1",
+        },
+    ]
 //start function to play
 
 //event listener for click
@@ -31,3 +76,9 @@ countdown();
 //Score= points earned * time remaining
 
 //set up new end page for high scores
+
+}
+
+playGame();
+
+
