0136cc71f6d109685b650a22c5a983c7817b72b9
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-02-26 12:34:13 -0600

    pseudocode js game file

---
+++ assets/game.js
@@ -0,0 +1,19 @@
+// select question and choice el from html
+
+// select points and time remaining in score-display
+
+// create question array with question/choices/answer
+
+//start function to play
+
+// timer starts
+
+//event listener for click
+
+//correct, go to next question, increment score
+
+//wrong, subtract 5 seconds, choice hidden
+
+//Score= points earned * time remaining
+
+//set up new end page for high scores
